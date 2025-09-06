944fbfbb36965ca81bda61d2d8c9de53baebbde6
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2025-09-06 08:22:53 +0200

    Rename Core App
    Rename core app from django_forenlims_core following Django conventions.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,4 +1,3 @@
-# ruff: noqa: E501
 """
 WSGI config for django_forenlims_core project.
 
@@ -12,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_forenlims_core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
