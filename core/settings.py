a2c622ad02769d4f86cbf7bd1c035716b6d27e08
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2025-09-06 10:25:35 +0200

    Add Accounts App
    Add Accounts App with Custom User Model.
    Run manage.py startapp accounts and include it in INSTALLED_APPS.
    Set AUTH_USER_MODEL to 'accounts.CustomUser' in settings.py.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication
+AUTH_USER_MODEL = 'accounts.CustomUser'
