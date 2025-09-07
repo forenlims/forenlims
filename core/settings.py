d4d5c6ed1c66d66f34f5b5952b918e622efeab71
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2025-09-07 11:28:56 +0200

    Add .env Support for Environment Variables
    - Added `django-environ` package to manage environment variables.
    - Created a `.env.example` file to illustrate required environment variables.
    - Updated `core/settings.py` to use environment variables.
    - DEBUG and SECRET_KEY are now sourced from the `.env` file.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -10,23 +10,29 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'), overwrite=True)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# ruff: noqa: S105
-# For now, Ignore the security warning during linting.
-SECRET_KEY = 'django-insecure-fg7^6m(2)^5!*g^!c0=!u!79v8d=w(=(n1gy6q6wretj96^dxn'
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
