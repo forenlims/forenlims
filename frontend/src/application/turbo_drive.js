74877e8ec0ce331bd8d2f7eb3e1b23edcb6e4926
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2025-09-18 20:48:31 +0200

    Rename Entry Point to turbo_drive
    We rename the entry point for the frontend from "app" to "turbo_drive" to
    better reflect its purpose. This includes renaming the JS and CSS
    files as well.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- frontend/src/application/turbo_drive.js
+++ frontend/src/application/turbo_drive.js
@@ -1,6 +1,6 @@
 
 // This is the style entry file
-import "../styles/index.css";
+import "../styles/turbo_drive.css";
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 
@@ -8,3 +8,7 @@ const application = Application.start();
 
 const context = require.context("../controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
+
+window.document.addEventListener("DOMContentLoaded", function () {
+  window.console.log("dom ready");
+});
