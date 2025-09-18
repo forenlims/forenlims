35ef0918e1a5cc8a2d8800b6d09a56b5dcc12f9e
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2025-09-18 21:11:39 +0200

    Tooling/hotwire_turbo (#19)
    * Remove Autogenerated Files
    
    Remove files that were generated by the webpack boilerplate and are
    not needed in our application.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
    
    * Rename Entry Point to turbo_drive
    
    We rename the entry point for the frontend from "app" to "turbo_drive" to
    better reflect its purpose. This includes renaming the JS and CSS
    files as well.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
    
    * Install Hotwired Turbo
    
    npm install @hotwired/turbo
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
    
    * Add Turbo To the frontend
    
    Import Turbo and create an event listener for turbo:load.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- frontend/src/application/turbo_drive.js
+++ frontend/src/application/turbo_drive.js
@@ -1,6 +1,7 @@
 
 // This is the style entry file
-import "../styles/index.css";
+import "../styles/turbo_drive.css";
+import "@hotwired/turbo";
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 
@@ -8,3 +9,10 @@ const application = Application.start();
 
 const context = require.context("../controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
+
+window.document.addEventListener("DOMContentLoaded", function () {
+  window.console.log("dom ready");
+});
+document.addEventListener('turbo:load', function () {     // new
+  console.log('turbo:load');
+});
