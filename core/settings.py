19c39de782abcf776b49990e6d1728f28d812e00
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2025-10-11 08:19:00 +0200

    Fix .env Branch Mismatch
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -30,10 +30,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'), overwrite=True)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = []
 
